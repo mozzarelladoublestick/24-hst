79070630b6db4c91e9b284299c688d250d5eb3f9
mozzarelladoublestick <s2110238075@fhooe.at>
2024-04-04 18:48:46 +0200

    add mobile styling

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -5,14 +5,14 @@
   >
     <img
       src="../icons/2024-03_oesm_hst24_logo_2c-weiss-rot.svg"
-      class="absolute left-1/3 z-0 w-96 top-20"
+      class="absolute left-1/2 md:left-1/3 z-0 w-40 md:w-96 top-20"
     />
     <div
-      class="uppercase h-screen py-64 text-9xl text-white font-bold text-center z-50 relative"
+      class="uppercase h-screen py-64 text-5xl md:text-9xl text-white font-bold text-center z-50 relative"
     >
       COMING
-      <p class="my-8 z-50">SOON</p>
-      <p class="m-6 text-orange font-light text-4xl">stay tuned</p>
+      <p class="my-2 md:my-8 z-50">SOON</p>
+      <p class="m-6 text-orange font-light text-2xl md:text-4xl">stay tuned</p>
     </div>
   </div>
 </template>
