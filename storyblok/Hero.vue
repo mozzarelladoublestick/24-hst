5900c56ffb2886f592ef55c0495013cd04c445a9
mozzarelladoublestick <s2110238075@fhooe.at>
2024-04-04 19:56:13 +0200

    mobile changes

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -1,18 +1,18 @@
 <template>
   <div
     v-editable="blok"
-    class="relative pt-32 w-full m-0 bg-cover hero-gradient h-screen overflow-hidden"
+    class="relative md:pt-32 w-full m-0 bg-cover hero-gradient h-screen overflow-hidden"
   >
     <img
       src="../icons/2024-03_oesm_hst24_logo_2c-weiss-rot.svg"
-      class="absolute left-1/2 md:left-1/3 z-0 w-40 md:w-96 top-20"
+      class="absolute left-1/2 md:left-1/3 z-0 w-40 md:w-80 top-20"
     />
     <div
-      class="uppercase h-screen py-64 text-5xl md:text-9xl text-white font-bold text-center z-50 relative"
+      class="flex items-center flex-col justify-center md:block uppercase h-screen py-64 text-5xl md:text-8xl text-white font-bold text-center z-50 relative"
     >
       COMING
-      <p class="my-2 md:my-8 z-50">SOON</p>
-      <p class="m-6 text-orange font-light text-2xl md:text-4xl">stay tuned</p>
+      <p class="my-0 md:my-8 z-50">SOON</p>
+      <p class="m-4 text-orange font-light text-2xl md:text-4xl">stay tuned</p>
     </div>
   </div>
 </template>
