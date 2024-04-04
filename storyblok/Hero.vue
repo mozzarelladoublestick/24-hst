90dd93b687dc7c23c47ccac74da1f3557d505a33
mozzarelladoublestick <s2110238075@fhooe.at>
2024-04-04 20:07:35 +0200

    add GSAP

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -10,11 +10,47 @@
     <div
       class="flex items-center flex-col justify-center md:block uppercase h-screen py-64 text-5xl md:text-8xl text-white font-bold text-center z-50 relative"
     >
-      COMING
-      <p class="my-0 md:my-8 z-50">SOON</p>
+      <p id="coming">COMING</p>
+      <p id="scramble" class="my-0 md:my-8 z-50">SOON</p>
       <p class="m-4 text-orange font-light text-2xl md:text-4xl">stay tuned</p>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+
+onMounted(() => {
+  gsap.registerPlugin(TextPlugin);
+
+  const comingWords = ["coming", "soon"]; // Define the array of words for "COMING"
+  const soonWords = ["soon", "coming"]; // Define the array of words for "SOON"
+
+  function scrambleWords(words, elementId) {
+    words.forEach((word, index) => {
+      setTimeout(() => {
+        scramble(word, elementId);
+      }, index * 3000); // Delay each word by its index times 2000 milliseconds
+    });
+
+    // Call the function again after a delay to continuously scramble the words
+    setTimeout(() => {
+      scrambleWords(words, elementId);
+    }, words.length * 3000); // Delay the function call by the total time it takes to scramble all words
+  }
+
+  // Call the function initially to start the scrambling process for both paragraphs
+  scrambleWords(comingWords, "coming");
+  scrambleWords(soonWords, "scramble");
+});
+
+function scramble(word, elementId) {
+  gsap.to(`#${elementId}`, {
+    duration: 1,
+    text: word,
+    ease: "none",
+  });
+}
+</script>
