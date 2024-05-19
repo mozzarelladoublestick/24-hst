5e0405b745348c459f82ffd8633dff227657ded5
mozzarelladoublestick <s2110238075@fhooe.at>
2024-05-19 20:16:08 +0200

    videos!

---
+++ storyblok/Video.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-editable="blok" class="max-w-80">
+    <iframe
+      :src="blok.video_link.url"
+      title="Lost &amp; Found -  Auf der Suche nach Zukunft - Talk // Q&amp;A // Live-Art - Dr. Christian Bensel"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+      class="aspect-video"
+    ></iframe>
+    <div class="text-left mt-6">
+      <h3 class="text-xl">{{ blok.title }}</h3>
+      <h5 class="text-md">{{ blok.description }}</h5>
+      <h5 class="text-sm text-orange">{{ blok.info }}</h5>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({ blok: Object });
+</script>
---
+++ storyblok/Videos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    v-editable="blok"
+    class="container flex flex-wrap gap-20 py-12 mx-auto justify-center"
+  >
+    <StoryblokComponent
+      v-for="video in blok.videos"
+      :key="video._uid"
+      :blok="video"
+    />
+  </div>
+</template>
+<script setup>
+defineProps({ blok: Object });
+</script>
